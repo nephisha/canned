df0250cb142d2480561d07d82c5ea7dbd364d420
Philipp Fehre <philipp@fehre.co.uk>
2015-01-08 09:36:33 +0000

    added test for relative path resolving
    the resolving of the relative path fails to work correctly on the cli as
    reported in #51

--- test/bin_test.sh
+++ test/bin_test.sh
@@ -6,6 +6,7 @@
 CPID=$!
 sleep 1
 assert "lsof -i:8765 | grep node | awk '{print \$1}'" "node"
+assert 'curl -sL -w "%{http_code}" http://127.0.0.1:8765/search' "200"
 kill $CPID
 
 assert_end examples
